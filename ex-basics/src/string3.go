b6183dc61fadff751536195f10da478f37932a88
NanXiao <xn212516@163.com>
2013-11-21 10:00:11 +0800

    Add length check of the string

--- ex-basics/src/string3.go
+++ ex-basics/src/string3.go
@@ -2,11 +2,18 @@ package main
 
 import (
 	"fmt"
+	"unicode/utf8"
 )
+
+const minStrLen = 7
 
 func main() {
 	s := "Шла Саша по шоссе"
 	r := []rune(s)
+	if utf8.RuneCount([]byte(s)) < minStrLen {
+		fmt.Printf("The length of string is shorter than %d\n", minStrLen)
+		return
+	}
 	copy(r[4:4+3], []rune("abc"))
 	fmt.Printf("Before: %s\n", s);
 	fmt.Printf("After : %s\n", string(r))
